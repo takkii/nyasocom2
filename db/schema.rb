1de51b6f7282bb2d762930c357def2fcd2416641
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-08-13 13:41:47 +0900

    Update.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_030537) do
+ActiveRecord::Schema.define(version: 2021_03_10_030536) do
 
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
