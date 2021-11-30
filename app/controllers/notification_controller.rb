ebef9e6021ae861cea916530d2659776db34ea24
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-11-30 18:07:02 +0900

    Update, days change created_at.

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -1,5 +1,5 @@
 class NotificationController < BlogsController
   def list
-    @blogs = Kaminari.paginate_array(Blog.search(params[:search]).order(days: :desc)).page(params[:page])
+    @blogs = Kaminari.paginate_array(Blog.search(params[:search]).order(created_at: :desc)).page(params[:page])
   end
 end
