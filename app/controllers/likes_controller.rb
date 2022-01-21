e3bc01230a95d5e8438691e5c7ef2ada916e4f4f
Takayuki Kamiyama <karuma.reason@gmail.com>
2022-01-21 12:06:58 +0900

    Update.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,10 +3,12 @@ class LikesController < ApplicationController
 
   def create
     Like.create(user_id: current_user.id, blog_id: params[:id])
+    redirect_to notification_list_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, blog_id: params[:id]).destroy
+    redirect_to notification_list_path
   end
 
   private
