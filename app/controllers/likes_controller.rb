678bdbd4d3489fc81f6b5e7b39654487e1dbce01
Takayuki Kamiyama <karuma.reason@gmail.com>
2022-02-03 14:03:36 +0900

    Update, ajax likes.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,19 @@
 class LikesController < ApplicationController
+  before_action :blog_params
+
   def create
     Like.create(user_id: current_user.id, blog_id: params[:id])
-    redirect_to notification_list_path
+    #redirect_to notification_list_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, blog_id: params[:id]).destroy
-    redirect_to notification_list_path
+    #redirect_to notification_list_path
+  end
+
+  private
+
+  def blog_params
+    @blog = Blog.find(params[:id])
   end
 end
